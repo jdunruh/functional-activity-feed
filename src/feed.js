4fd3dcbfd329e0229cbeb38c4b101240c605b1e8
0P <j_unruh@hotmail.com>
2015-09-23 20:48:32 -0600

    Refactor into a functional programming style

--- src/feed.js
+++ src/feed.js
@@ -1,14 +1,15 @@
-var Store = require('./store.js')
+var Store = require('./../dist/app.js')
 var moment = require('moment')
 
+/*
 $( () => {
 
   new Store().load().then( (store) => {
 
     store.activityFeed().forEach((entry) => {
       if (entry.type === 'user') {
-        let article = $('<article class="user">')
-          .append(`<p class="date">${moment(new Date(entry.record.created_at)).fromNow()}</p>`)
+        let article = $('<article class="user">')`<p class="date">${moment(new Date(entry.
+          .append(record.created_at)).fromNow()}</p>`)
           .append(`<i class="fa fa-user"></i>`)
           .append(`<h1>${entry.record.full_name}</h1>`)
           .append(`<p class="info">${entry.record.email}</p>`)
@@ -33,3 +34,39 @@ $( () => {
   })
 
 })
+*/
+
+var renderHeader = function(articleClass, entry) {
+  return '<article class="' + articleClass + '">' + '<p class="date">' +
+      moment(new Date(entry.created_at)).fromNow() + '</p>';
+};
+var renderArticle = function(articleClass, articleFormatter, entry) {
+  return renderHeader(articleClass, entry) +
+      articleFormatter(entry);
+  };
+
+var userFormatter = function(user) {
+  return '<i class="fa fa-user"></i>' +
+  '<h1>' + entry.full_name + '</h1>` +' +
+  '`<p class="info">' + entry.record.email + '</p>';
+};
+
+var footerFormatter = function(entry) {
+  return '<p class="info">Created by ' + entry.creator.full_name + ' ' + entry.creator.email + '</p>';
+};
+
+var companyFormatter = function(company) {
+  return '<i class="fa fa-building"></i>' +
+  '<h1>' + company.name + '</h1>' +
+  footerFormatter(company);
+};
+
+var photoFormatter = function(photo) {
+  return '<i class="fa fa-photo"></i>' +
+        '<img src="' + photo.record.url + '">' +
+        footerFormatter(photo);
+};
+
+var insertIntoBody = function(article) {
+  $('body').append(article);
+};
